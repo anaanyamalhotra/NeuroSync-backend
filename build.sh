3fc32a8cfe99799c8b164d3581222a490a2ac1eb
anaanyamalhotra <ananyamalhotra3110@gmail.com>
2025-05-30 21:19:39 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+echo "Installing requirements..."
 pip install -r requirements.txt
+
+echo "Downloading TextBlob corpora..."
 python -m textblob.download_corpora
+
+echo "Build completed."
