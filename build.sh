f29f0e1d4ff3c3a8f7f009c4428089e9f6a2177e
anaanyamalhotra <ananyamalhotra3110@gmail.com>
2025-05-30 21:51:35 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing requirements..."
-pip install -r requirements.txt
-
-echo "Downloading TextBlob corpora..."
-python -m textblob.download_corpora
-
-echo "Build completed."
