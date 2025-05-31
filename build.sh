82e7611d48be7a43a1760a3f656e5b9dcb535e3e
anaanyamalhotra <ananyamalhotra3110@gmail.com>
2025-05-30 22:41:43 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+# Step 1: Install Python packages from requirements.txt
+pip install -r requirements.txt
+
+# Step 2: Make nltk_data directory
 mkdir -p nltk_data
+
+# Step 3: Download TextBlob corpora
 python3 -m textblob.download_corpora
+
+# Step 4: Download NLTK corpora
 python3 -m nltk.downloader -d ./nltk_data punkt averaged_perceptron_tagger wordnet brown movie_reviews
+
