787dde8e277bf5efb17ba828d8f0cf576a855326
anaanyamalhotra <ananyamalhotra3110@gmail.com>
2025-05-31 21:33:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ from vector_store import load_metadata
 from textblob import TextBlob
 from generator import infer_life_stage_from_text
 
-df = pd.DataFrame([my_dict]) 
 
 from textblob import download_corpora
 nltk_data_path = os.path.join(os.path.dirname(__file__), "nltk_data")
