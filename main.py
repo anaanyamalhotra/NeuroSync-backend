d187004bcc25d6e3df33495c7c1e324bd2717b04
anaanyamalhotra <ananyamalhotra3110@gmail.com>
2025-06-01 20:46:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -354,7 +354,7 @@ def get_twins(
             if (not gender or m.get("gender") == gender)
             and (not life_stage or m.get("life_stage") == life_stage)
             and (not age_range or m.get("age_range") == age_range)
-            and (not top_neurotransmitters or m.get("top_neurotransmitters") == top_neurotransmitters
+            
             and (not user_id or m.get("user_id") == user_id)
         ]
 
